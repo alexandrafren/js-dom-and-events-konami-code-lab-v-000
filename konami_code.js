11edeb7cacfae84af309a252b4e63c16d54f4453
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-16 23:35:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -13,3 +13,5 @@ function init(e) {
     index = 0;
   }
 }
+
+body.addListenerEvent('keydown', init())
