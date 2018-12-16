b6f3229730e06c214687eff72eb76896b11cd6d8
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-16 23:32:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
+let index = 0;
+function init(e) {
   // Write your JavaScript code inside the init() function
-
+  const key = parseInt(e.which);
+  if (key === code[index]){
+    index++;
+    if (index === code.length) {
+      alert("Congratulations");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
